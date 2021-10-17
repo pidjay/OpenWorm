7f733ac914f49be1ff6e53a31174a4ec9e485d06
Pierre-Jean Quilleré <pj.quillere@gmail.com>
2021-10-17 12:47:36 -0700

    Display empty search view controller

---
+++ OpenWorm/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  OpenWorm
+//
+//  Created by Pierre-Jean Quilleré on 2021-10-17.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
