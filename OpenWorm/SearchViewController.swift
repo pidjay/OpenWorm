9d27276f843992f50e8eb36626dcfa19b0ba74a8
Pierre-Jean Quilleré <pj.quillere@gmail.com>
2021-10-17 13:04:50 -0700

    Fix background color in search view controller

--- OpenWorm/SearchViewController.swift
+++ OpenWorm/SearchViewController.swift
@@ -13,6 +13,9 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
         configureEmptyStateView()
     }
     
