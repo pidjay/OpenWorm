0bbc80acc6cf17f03ba66c993a4a52aa72daec06
Pierre-Jean Quilleré <pj.quillere@gmail.com>
2021-10-17 21:07:47 -0700

    Remove empty state TODO

--- OpenWorm/Screens/SearchViewController.swift
+++ OpenWorm/Screens/SearchViewController.swift
@@ -44,7 +44,6 @@ class SearchViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         #warning("TODO: loading state")
-        #warning("TODO: empty state when no content")
         
         configureCollectionView()
         configureEmptyStateView()
